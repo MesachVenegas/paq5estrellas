7da6087aa9fb31661ec87fe4728e872145981075
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-09-11 21:24:01 -0700

    changes: complete infobar mobile view

--- src/components/SocialBar/SocialBar.jsx
+++ src/components/SocialBar/SocialBar.jsx
@@ -1,24 +1,25 @@
 import './style.css'
+import { social } from './../../utils/constants';
 
 const SocialBar = () => {
     return (
         <div className="info_bar">
             <div className="social_container">
-                <a href="#" target="_blank" className="social_item">
+                <a href={social.facebook} target="_blank" rel="noreferrer" >
                     <i className="fa-brands fa-facebook-f"></i>
                 </a>
-                <a href="#" target="_blank" className="social_item">
+                <a href={social.whatsapp} target="_blank" rel='noreferrer' >
                     <i className="fa-brands fa-whatsapp"></i>
                 </a>
-                <a href="#" target="_blank" className="social_item">
+                {/* <a href="#" target="_blank" rel='noreferrer' >
                     <i className="fa-brands fa-instagram"></i>
-                </a>
-                <a href="#" target="_blank" className="social_item">
+                </a> */}
+                <a href={social.tiktok} target="_blank" rel="noreferrer">
                     <i className="fa-brands fa-tiktok"></i>
                 </a>
             </div>
             <div className="search_container">
-                <input type="text"  className="search_input" />
+                <input type="text"  className="search_input" placeholder='rastrear mi pedido...'/>
                 <button className="search_btn">
                     <i className="fa-solid fa-magnifying-glass"></i>
                 </button>
