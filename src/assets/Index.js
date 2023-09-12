6bb63f957b903b4979d855ccc46344742a2fa7fc
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-09-11 20:22:54 -0700

    add: indexer assets

---
+++ src/assets/Index.js
@@ -0,0 +1,34 @@
+import calculator from './calculator-solid.svg';
+import zip from './envelopes-bulk-solid.svg';
+import menu_btn from './bars-solid.svg';
+import facebook from './facebook-f.svg';
+import check from './check-solid.svg';
+import Logo_svg from './Logo.svg';
+import logo_png from './logo_no_bg.png'
+import instagram from './instagram.svg';
+import search from './magnifying-glass-solid.svg';
+import location from './map-location-dot-solid.svg';
+import send from './paper-plane-solid.svg';
+import layer1 from './paqueteria_layer.jpg';
+import phone from './phone-solid.svg';
+import tiktok from './tiktok.svg';
+import whatsapp from './whatsapp.svg';
+
+
+export {
+    menu_btn,
+    calculator,
+    zip,
+    facebook,
+    check,
+    Logo_svg,
+    logo_png,
+    instagram,
+    search,
+    location,
+    send,
+    layer1,
+    phone,
+    tiktok,
+    whatsapp,
+}
