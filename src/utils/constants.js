80d72c83d96ca7e8743680190fa2732f11238ef1
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-09-11 21:23:26 -0700

    add: constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const social = {
+    facebook : "https://www.facebook.com/paqueteria5estrellas?locale=es_LA",
+    whatsapp: "https://api.whatsapp.com/send?phone=5216632219250&text=Me%20interesa%20hacer%20un%20env%C3%ADo.%0A%F0%9F%93%A6%F0%9F%9B%A9%EF%B8%8F",
+    tiktok: "https://www.tiktok.com/@paqueteria5estrellas",
+    instagram: "",
+}
