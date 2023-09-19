cff2c637119773a19b6b40360b5527886f16d0e9
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-09-18 19:02:47 -0700

    add: iamges to carrousel to index

--- src/assets/Index.js
+++ src/assets/Index.js
@@ -9,11 +9,14 @@ import instagram from './instagram.svg';
 import search from './magnifying-glass-solid.svg';
 import location from './map-location-dot-solid.svg';
 import send from './paper-plane-solid.svg';
-import layer1 from './paqueteria_layer.jpg';
+import slider1 from './paqueteria_layer.jpg';
 import phone from './phone-solid.svg';
 import tiktok from './tiktok.svg';
 import whatsapp from './whatsapp.svg';
-
+import slider2 from './panfleto.jpg';
+import slider3 from './slider3.jpg';
+import slider4 from './slider4.jpg';
+import slider5 from './slider5.jpg';
 
 export {
     menu_btn,
@@ -27,7 +30,11 @@ export {
     search,
     location,
     send,
-    layer1,
+    slider1,
+    slider2,
+    slider3,
+    slider4,
+    slider5,
     phone,
     tiktok,
     whatsapp,
