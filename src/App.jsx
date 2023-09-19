69d17845a78d747f2ffc63ebd939ab17cf05f268
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-09-18 22:49:28 -0700

    add: home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SocialBar, Navbar} from './components/Index';
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 import './App.css'
 
 function App() {
