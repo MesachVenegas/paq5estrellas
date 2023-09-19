81aad18610c20acb14300a31111c36fdcdc41d41
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-09-18 19:03:13 -0700

    create: carrousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,11 +1,53 @@
-import { layer1 } from '../../assets';
+/* eslint-disable react/prop-types */
+import { useState } from 'react';
 import './style.css';
 
-const Carousel = () => {
+const Carousel = ({ images }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handlePrevious = () => {
+        setCurrentIndex(prevIndex =>
+            prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1
+        );
+    }
+
+    const handleNext = () => {
+        setCurrentIndex(prevIndex =>
+            prevIndex + 1 === images.length ? 0 : prevIndex + 1
+        );
+    }
+
     return (
-        <div className="slide_container">
-            <img src={layer1} alt="slide_1" />
-        </div>
+        <section className="carousel">
+            <div className="carousel-images">
+                {images.map((image, index) => (
+                    <img
+                        key={index}
+                        src={image}
+                        className={currentIndex === index ? "active" : "inactive"}
+                    />
+                ))
+                }
+            </div>
+            <div className="carousel-controls">
+                <button className='left-btn' onClick={handlePrevious}>
+                    <i className="fa-solid fa-chevron-left"></i>
+                </button>
+                <button className='right-btn' onClick={handleNext}>
+                    <i className="fa-solid fa-chevron-right"></i>
+                </button>
+            </div>
+            <div className="carousel-dots">
+                {images.map((_, index) => (
+                    <div
+                        key={index}
+                        className={`dot ${currentIndex === index ? "active" : ""}`}
+                    >
+                    </div>
+                ))
+                }
+            </div>
+        </section>
     );
 };
 
