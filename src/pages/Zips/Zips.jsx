52b2f5ec1e46275ac5a95596d05db55d10900edb
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-09-21 15:45:42 -0700

    changes: add request to get town details and colognes

--- src/pages/Zips/Zips.jsx
+++ src/pages/Zips/Zips.jsx
@@ -1,37 +1,64 @@
 import axios from "axios";
 import { states } from '../../utils/constants';
+import { useEffect, useState } from "react";
+import Colognes from "../../components/Colognes/Colognes";
 import './zips.css';
 
 const Zips = () => {
     // 285c0604-3066-4a26-b02b-a7704b2f0b0e
+    // https://api.copomex.com/query/get_municipio_por_estado/Aguascalientes?token=pruebas
+    const [currentState, setCurrentState] = useState('none');
+    const [currentTown, setCurrentTown] = useState('none');
+    const [towns, setTowns] = useState([]);
+
+    const handleForm = (data) => {
+        console.log(data);
+    }
+
+    const getTowns = () => {
+        if(currentState !== 'none'){
+            axios.get(`https://api.copomex.com/query/get_municipio_por_estado/${currentState}?token=pruebas`)
+                .then(res => setTowns(res.data.response.municipios))
+                .catch(err => console.error(err));
+        }
+    };
+
+    useEffect( () => {
+        getTowns();
+    },[currentState])
 
 
     return (
         <section className="container">
             <h2>Consulta aquí tu Código Postale</h2>
-            <form  className="form_consult">
+            <form  className="form_consult" onSubmit={handleForm}>
                 <h3>Ingresa los datos solicitados para obtener información sobre los códigos postales.</h3>
                 <div className="select_item">
                     <label htmlFor="state">Estado :</label>
-                    <select name="state" id="state">
-                        <option key="nonvalue" value="none" selected>seleccionar</option>
+                    <select name="state" id="state" defaultValue="seleccionar" onChange={(e) => setCurrentState(e.target.value)}>
+                        <option key="nonvalue"  value="none">seleccionar</option>
                         {
                             states.map(state => (
-                                <option key={state} value={state}>{state}</option>
+                                <option key={state} value={state}> {state}</option>
                             ))
                         }
                     </select>
                 </div>
                 <div className="select_item">
                     <label htmlFor="town">Delegación / Municipio :</label>
-                    <select name="town" id="town">
-                        <option value="test" selected>seleccionar</option>
+                    <select name="town" id="town" defaultValue="seleccionar un estado" onChange={(e) => setCurrentTown(e.target.value)}>
+                        <option key="nonvalue" value="none">selecciona un estado</option>
+                        {
+                            towns.map(town => (
+                                <option key={town} value={town}>{town}</option>
+                            ))
+                        }
                     </select>
                 </div>
                 <div className="select_item">
                     <label htmlFor="">Colonia :</label>
                     <select name="cologne" id="cologne">
-                        <option value="test" selected>seleccionar</option>
+                        <Colognes currentTown={currentTown}/>
                     </select>
                 </div>
                 <div className="control">
