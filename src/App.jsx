71971c0021a6917e718a2eec87b952645ea95921
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-09-21 17:46:21 -0700

    add: track route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SocialBar, Navbar, Footer} from './components/Index';
 import Zips from './pages/Zips/Zips';
 import Home from './pages/Home/Home';
+import Track from './pages/Rastreo/Track';
 import './App.css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='/zips' element={<Zips />}/>
+            <Route path='/track' element={<Track />}/>
         </Routes>
       </main>
       <Footer />
