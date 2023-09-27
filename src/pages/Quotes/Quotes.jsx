7445c0e3a39d9071499840f6449738083ca97b3b
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-09-27 03:15:28 -0700

    add: zip code validations

--- src/pages/Quotes/Quotes.jsx
+++ src/pages/Quotes/Quotes.jsx
@@ -1,6 +1,13 @@
+import { useForm } from 'react-hook-form';
 import './quotes.css';
 
 const Quotes = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = handleSubmit(data => {
+        console.log(data);
+    })
+
     return (
         <section className='container'>
             <div className="section_head">
@@ -12,21 +19,47 @@ const Quotes = () => {
                     <li>Da click en calcular.</li>
                 </ol>
             </div>
-            <form className="quote_form">
+            <form className="quote_form" onSubmit={onSubmit}>
                 <div className="form_group">
                     <label htmlFor="origin">Codigo postal origen:</label>
-                    <input type="text" id='origin'/>
+                    <div className="input_box">
+                        <input
+                            type="text"
+                            id='origin'
+                            {...register("origin", {
+                                required: true,
+                                pattern: /^[0-9]{5}$/
+                            })}
+                        />
+                        {
+                            errors.origin?.type === "required" && <small className='error_msg'>Ingrese un codigo postal primero</small>
+                        }
+                        {
+                            errors.origin?.type === "pattern" && <small className='error_msg'>Ingrese un codigo postal valido</small>
+                        }
+                    </div>
                 </div>
                 <div className="form_group">
                     <label htmlFor="destiny">Codigo postal destino:</label>
-                    <input type="text" id='destiny'/>
+                    <div className="input_box">
+                        <input type="text" id='destiny' {...register("destiny", {
+                            required: true,
+                            pattern: /^[0-9]{5}$/
+                        })} />
+                        {
+                            errors.destiny?.type === "required" && <small className='error_msg'>Ingrese un codigo postal primero</small>
+                        }
+                        {
+                            errors.destiny?.type === "pattern" && <small className='error_msg'>Ingrese un codigo postal valido</small>
+                        }
+                    </div>
                 </div>
                 <div className="radio_container">
                     <p>Tipo:</p>
                     <div className="radio_group">
-                        <input type="radio" id='upon' value='upon' checked/>
+                        <input type="radio" id='upon' value='upon' />
                         <label htmlFor="upon">Sobre</label>
-                        <input type="radio" id='package' value='package'/>
+                        <input type="radio" id='package' value='package' />
                         <label htmlFor="package">Paquete</label>
                     </div>
                 </div>
@@ -37,19 +70,19 @@ const Quotes = () => {
                     <div className="form_group-package">
                         <div className="form_group-package-item">
                             <label htmlFor="weight">Peso en Kg:</label>
-                            <input type="text" id='weight' />
+                            <input type="text" id='weight' {...register("weight")} />
                         </div>
                         <div className="form_group-package-item">
                             <label htmlFor="high">Alto en cms:</label>
-                            <input type="text" id='hight' />
+                            <input type="text" id='high'  {...register("high")} />
                         </div>
                         <div className="form_group-package-item">
                             <label htmlFor="width">Ancho en cms:</label>
-                            <input type="text" id='width' />
+                            <input type="text" id='width' {...register("width")} />
                         </div>
                         <div className="form_group-package-item">
                             <label htmlFor="long">Largo en cms:</label>
-                            <input type="text" id='long' />
+                            <input type="text" id='long'  {...register("long")} />
                         </div>
                     </div>
                 </div>
