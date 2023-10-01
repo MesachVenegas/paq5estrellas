097e0f392b4998fafe1babb8b11c44ad2d79e9ea
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-10-01 14:39:33 -0700

    add: table component

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -2,11 +2,13 @@ import SocialBar from "./SocialBar/SocialBar";
 import Carousel from "./carousel/Carousel";
 import Navbar from "./navbar/Navbar";
 import Footer from "./footer/Footer";
+import Table from "./quoteTable/Table";
 
 
 export {
     SocialBar,
     Navbar,
     Carousel,
-    Footer
+    Footer,
+    Table
 }
