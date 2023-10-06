41e113edd3ec0c9045a8844c25142566d009dfd0
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-10-05 22:34:34 -0700

    changes: prices and column show

--- src/components/quoteTable/Table.jsx
+++ src/components/quoteTable/Table.jsx
@@ -6,45 +6,53 @@ const Table = ({ weight, type, places }) => {
     const [landGuide, setLandGuide] = useState(0);
     const [airGuide, setAirGuide] = useState(0);
     const [landExtra, setLandExtra] = useState(0);
+    const [showAir, setShowAir] = useState('normal')
     const peso = parseInt(weight)
 
     const getLandGuide = () => {
-        if(!type){
-            setLandGuide(525);
+        if (!type) {
+            setLandGuide(490);
             setAirGuide(525);
             setLandExtra(0);
-        } else if(peso > 0 && peso == 1 ){
+        } else if (peso >= 0 && peso <= 5) { // de 0 a 5 kgs
             setLandExtra(0)
-            setLandGuide(525);
-        } else if(peso >= 2 && peso <= 5){
+            setLandGuide(490);
+        } else if (peso >= 6 && peso <= 10) { // de 6 a 10 kgs
             setLandExtra(0)
-            setLandGuide(675);
-        } else if(peso >= 6 && peso <= 10){
+            setLandGuide(700)
+        } else if (peso >= 11 && peso <= 15) { // de 11 a 15 kgs
             setLandExtra(0)
-            setLandGuide(907)
-        } else if (peso >= 11 && peso <= 15){
+            setLandGuide(840);
+        } else if (peso >= 16 && peso <= 20) { // de 16 a 20 kgs
             setLandExtra(0)
-            setLandGuide(1155);
-        } else if (peso >= 16 && peso <= 20){
+            setLandGuide(1007.50);
+        } else if (peso >= 21 && peso <= 25) { // de 21 a 25 kgs
             setLandExtra(0)
-            setLandGuide(1320);
-        } else if (peso >= 21 && peso <= 25){
+            setLandGuide(1162.50);
+        } else if (peso >= 26 && peso <= 30) { // de 26 a 30 kgs
             setLandExtra(0)
-            setLandGuide(1530);
-        } else if (peso >= 26 && peso <= 30){
+            setLandGuide(1317);
+        } else if (peso >= 31 && peso <= 35) { // de 31 a 35 kgs
             setLandExtra(0)
-            setLandGuide(1700);
-        } else if (peso >= 31 && peso <= 35){
+            setLandGuide(1395);
+        }
+        else if (peso >= 36 && peso <= 40) { // de 36 a 40 kgs
             setLandExtra(0)
-            setLandGuide(1925);
-        } else if (peso >= 36 && peso <= 40){
+            setLandGuide(1472.50);
+        }
+        else if (peso >= 41 && peso <= 45) { // de 41 a 45 kgs
             setLandExtra(0)
-            setLandGuide(2100);
-        } else if (peso == 41){
+            setLandGuide(1627.50);
+        }
+        else if (peso >= 46 && peso <= 50) { // de 46 a 50 kgs
             setLandExtra(0)
-            setLandGuide(2275);
-        } else if (peso >= 42 ){
-            const extraWeight = peso - 41;
+            setLandGuide(1782.50);
+        }
+        else if (peso == 50) { // 2217.50 x 60kgs
+            setLandExtra(0)
+            setLandGuide();
+        } else if (peso >= 52) {
+            const extraWeight = peso - 50;
             console.log(extraWeight);
             setLandExtra(extraWeight * 43.50)
         }
@@ -70,14 +78,20 @@ const Table = ({ weight, type, places }) => {
         } else if (peso >= 36 && peso <= 40) {
             setAirGuide(2100);
         } else if (peso > 40) {
-            setAirGuide(undefined);
+            setAirGuide(0);
         }
     }
 
-    useEffect( () => {
+    useEffect(() => {
         getLandGuide();
         getAirGuide();
-    },[weight])
+
+        if (weight > 40) {
+            setShowAir('none')
+        }else {
+            setShowAir('flex')
+        }
+    }, [weight])
 
 
     return (
@@ -113,24 +127,24 @@ const Table = ({ weight, type, places }) => {
                     <tr>
                         <th className='concept'>Conceptos</th>
                         <th className='send_type'>Terrestre</th>
-                        <th className='send_type' >Aéreo</th>
+                        <th className='send_type send_air' style={{ display: `${showAir}` }} >Aéreo</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td>Peso</td>
                         <td className='land'>{weight ? `${weight} kgs` : `No Aplica`}</td>
-                        <td className='air'>{weight ? `${weight} kgs` : `No Aplica`}</td>
+                        <td className='air' style={{display: `${showAir}`}} >{weight ? `${weight} kgs` : `No Aplica`}</td>
                     </tr>
                     <tr>
                         <td>Coste de Guía</td>
                         <td className='land'>{`$ ${landGuide.toFixed(2)}`}</td>
-                        <td className='air'>{airGuide ? `$ ${airGuide.toFixed(2)}` : "Peso excedido"}</td>
+                        <td className='air' style={{ display: `${showAir}` }} >{airGuide ? `$ ${airGuide.toFixed(2)}` : "Peso excedido"}</td>
                     </tr>
                     <tr>
                         <td>Coste de peso excedente</td>
                         <td className='land'>{`$ ${landExtra.toFixed(2)}`}</td>
-                        <td className='air'>$ 0.00</td>
+                        <td className='air' style={{ display: `${showAir}` }} >$ 0.00</td>
                     </tr>
                     <tr>
                         <td className='total_ceil'>
@@ -138,13 +152,13 @@ const Table = ({ weight, type, places }) => {
                             <span>(Cotización Aproximada)</span>
                         </td>
                         <td>$ {(landGuide + landExtra).toFixed(2)}</td>
-                        <td>$ {airGuide.toFixed(2)}</td>
+                        <td className='air' style={{ display: `${showAir}` }} >$ {airGuide.toFixed(2)}</td>
                     </tr>
                 </tbody>
             </table>
             <ul className='notes'>
                 <li>*Costos de embalaje y tramites ya incluidos</li>
-                <li>*IVA incluido</li>
+                <li>*IVA no incluido</li>
                 <li>*Se aplica peso volumétrico cuando este es superior al peso físico</li>
                 <li>*En caso de que sea zona extendida puede aplicar cargo extra en sucursal</li>
                 <li>*Los sobres no de deben sobre pasar la cantidad de 30 hojas</li>
