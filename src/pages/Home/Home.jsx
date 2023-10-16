24b8ebdc47817683d7f8bcb2fa5ecbcbe08bdead
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-10-16 10:48:48 -0700

    changes: jpg to webp format

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import { Carousel } from '../../components/Index';
 import { Link } from 'react-router-dom';
-import slider1 from '../../assets/paqueteria_layer.jpg';
-import slider3  from '../../assets/slider3.jpg';
-import slider4  from '../../assets/slider4.jpg';
-import slider5  from '../../assets/slider5.jpg';
+import slider1 from '../../assets/paqueteria_layer.webp';
+import slider3  from '../../assets/slider3.webp';
+import slider4  from '../../assets/slider4.webp';
+import slider5  from '../../assets/slider5.webp';
 import './home.css'
 
 const Home = () => {
