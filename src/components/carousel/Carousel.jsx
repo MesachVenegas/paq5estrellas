677eab533d03759ffbc7047d1c2ef18f781f9e6d
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-10-17 16:01:35 -0700

    changes: carousel to bootsrap component

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,55 +1,40 @@
 /* eslint-disable react/prop-types */
 import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
-const Carousel = ({ images }) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
+function ControlledCarousel({ images }) {
+    const [index, setIndex] = useState(0);
 
-    const handlePrevious = () => {
-        setCurrentIndex(prevIndex =>
-            prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1
-        );
-    }
-
-    const handleNext = () => {
-        setCurrentIndex(prevIndex =>
-            prevIndex + 1 === images.length ? 0 : prevIndex + 1
-        );
-    }
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
 
     return (
-        <section className="carousel">
-            <div className="carousel-images">
-                {images.map((image, index) => (
-                    <img
-                        key={index}
-                        src={image}
-                        className={currentIndex === index ? "active" : "inactive"}
-                        alt='carrusel_aviso'
-                    />
-                ))
+        <section className='carousel'>
+            <Carousel
+                className='carousel-images'
+                activeIndex={index}
+                onSelect={handleSelect}
+            >
+                {
+                    images.map( (img, index) => (
+                        <Carousel.Item key={`img-${index}`} >
+                            <img
+                                key={index}
+                                className="img_item"
+                                src={img}
+                                width="100%"
+                                height="100%"
+                                alt={`${index}-slide`}
+                            />
+                        </Carousel.Item>
+                    ))
                 }
-            </div>
-            <div className="carousel-controls">
-                <button className='left-btn' onClick={handlePrevious}>
-                    <i className="fa-solid fa-chevron-left"></i>
-                </button>
-                <button className='right-btn' onClick={handleNext}>
-                    <i className="fa-solid fa-chevron-right"></i>
-                </button>
-            </div>
-            <div className="carousel-dots">
-                {images.map((_, index) => (
-                    <div
-                        key={index}
-                        className={`dot ${currentIndex === index ? "active" : ""}`}
-                    >
-                    </div>
-                ))
-                }
-            </div>
+            </Carousel>
         </section>
     );
-};
+}
 
-export default Carousel;
+export default ControlledCarousel;
