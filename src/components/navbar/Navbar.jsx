5a34dd0d83bbb22db035bce71b9e8d17d5029855
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-10-19 14:58:00 -0700

    add: function to close menu on link click

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,17 +11,24 @@ const Navbar = () => {
         menu.classList.toggle('open');
     }
 
+    const closeMenu = () => {
+        let menu_btn = document.querySelector('.menu_btn');
+        const menu = document.querySelector('.menu');
+        menu_btn.classList.remove('open');
+        menu.classList.remove('open');
+    }
+
     return (
         <nav>
             <div className="nav_container">
                 <Link to="/" className="brand">
-                    <img src={Logo_svg} alt="brand_logo" width="100px" height="100%"/>
+                    <img src={Logo_svg} alt="brand_logo"/>
                 </Link>
                 <ul className="menu">
-                    <li><Link to="/">Inicio</Link></li>
-                    <li><Link to="/about">Servicios</Link></li>
-                    <li><Link to="/about">Sobre Nosotros</Link></li>
-                    <li><Link to="/contact">Contactanos</Link></li>
+                    <li onClick={closeMenu}><Link to="/">Inicio</Link></li>
+                    <li onClick={closeMenu}><Link to="/about">Servicios</Link></li>
+                    <li onClick={closeMenu}><Link to="/about">Sobre Nosotros</Link></li>
+                    <li onClick={closeMenu}><Link to="/contact">Contactanos</Link></li>
                 </ul>
                 <div className="menu_btn" onClick={handleMenu}>
                     <span></span>
